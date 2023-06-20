19c3822a3836224ed501dc1bc6c9e0fab1f71b91
Julian Rojas <julianrojasgeo@gmail.com>
2023-06-20 18:10:47 -0500

    Education and experience components first progress

--- src/app/models/education.model.ts
+++ src/app/models/education.model.ts
@@ -1,8 +1,11 @@
+import { EducationTechnology } from "./education-technology.model";
+
 export interface Education {
   id?: string,
   institution: string,
   degree: string,
   imageUrl?: string,
+  education_tech?: EducationTechnology[],
   start_date: Date,
   end_date?: Date,
 }
--- src/app/models/experience.model.ts
+++ src/app/models/experience.model.ts
@@ -1,9 +1,13 @@
+
+import { ExperienceTechnology } from "./experience-technoloty.model";
+
 export interface Experience {
   id?: string,
   company: string,
   description: string,
   position: string,
   image_url?: string,
+  experience_tech?: ExperienceTechnology[],
   start_date: Date,
   end_date?: Date,
 }
