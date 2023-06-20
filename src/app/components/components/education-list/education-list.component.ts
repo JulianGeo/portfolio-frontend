76edc4de3e6012bc8a2ab00cf449d08c7bb19d37
Julian Rojas <julianrojasgeo@gmail.com>
2023-06-20 17:59:38 -0500

    Education service implemented in components

--- src/app/components/components/education-list/education-list.component.ts
+++ src/app/components/components/education-list/education-list.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { MatPaginatorIntl, PageEvent } from '@angular/material/paginator';
+import { Education } from 'src/app/models/education.model';
+import { EducationService } from 'src/app/services/education.service';
 
 @Component({
   selector: 'app-education-list',
@@ -6,5 +9,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./education-list.component.scss']
 })
 export class EducationListComponent {
+
+  l_education: Education[] = [];
+  total: number = this.l_education.length;
+  pageSlice: Education[] = [];
+
+  constructor(
+    private service: EducationService,
+    public _MatPaginatorIntl: MatPaginatorIntl
+  ) { }
+
+  ngOnInit(): void {
+    this._MatPaginatorIntl.itemsPerPageLabel = 'Items';
+
+    this.service.getAll().subscribe({
+      next: (education) => {
+        this.l_education = education,
+        this.total = this.l_education.length;
+        this.updatePageSlice(0,3);
+
+      },
+      error: (console.log),
+      complete: (console.log)
+    })
+  }
+
+  OnPageChange(event: PageEvent){
+    console.log(event)
+    this.updatePageSlice(event.pageIndex, event.pageSize);
+  }
+
+  private updatePageSlice(pageIndex: number, pageSize: number): void {
+    const startIndex = pageIndex * pageSize;
+    const endIndex = startIndex + pageSize;
+
+    this.pageSlice = this.l_education.slice(startIndex, endIndex);
+  }
 
 }
--- src/app/components/components/education/education.component.ts
+++ src/app/components/components/education/education.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Education } from 'src/app/models/education.model';
 
 @Component({
   selector: 'app-education',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./education.component.scss']
 })
 export class EducationComponent {
+
+  @Input() education: Education|undefined = {
+    institution: '',
+    degree: '',
+    imageUrl: '',
+    start_date: new Date(),
+    end_date: new Date(),
+  }
 
 }
